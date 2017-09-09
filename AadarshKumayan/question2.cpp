885db501e0303f7604ef7d7a48403454fbffa03c
AadarshKumayan <aadarsh1296@gmail.com>
2017-09-10 03:50:50 +0530

    update ques2 resume

--- AadarshKumayan/question2.cpp
+++ AadarshKumayan/question2.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main()
 {
-	long long int t[1000000],r[1000000],c[1000000],n=0,i,x=0,y,flag=0;
+	long long int t[1000000],r[1000000],c[1000000],n=0,i,x=0,y,flag=0;		//t-travel time r-ready time c-cancel time
 	char l='a';
-	while(l!='\n')
+	while(l!='\n')							//find out number of passengers
 	{
 		cin>>t[n++];
-		x+=t[n-1];
+		x+=t[n-1];							//minimum time will always contain sum of all travelling time
 		cin.get(l);
 	}
 	n++;
@@ -16,8 +16,12 @@ int main()
 	cin>>r[i];
 	for(i=0;i<n;i++)
 	cin>>c[i];
-	y=c[0];
-	for(i=1;i<n;i++)
+	for(i=r[0];i<=c[0];i++)						//max value between passenger 1 ready time and cancel time for which it will reach at ready time for passenger 2
+	{
+		if(i+t[0]<=r[1])
+		y=i;
+	}
+	for(i=1;i<n;i++)						//calculate waiting time for all passengers
 	{
 	y+=t[i-1];
 	if(y<r[i])
